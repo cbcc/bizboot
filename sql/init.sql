9a73ad6d558157571330a72c2af355c8942c132b
cbcc <827718623@qq.com>
2024-09-19 13:21:25 +0800

    docs: 修改初始化 sql 脚本

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,10 @@
+-- 创建数据库
+CREATE DATABASE IF NOT EXISTS `bizboot-system` DEFAULT CHARACTER SET = utf8mb4;
+
+Use `bizboot-system`;
+
 -- 用户表
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `username` VARCHAR(16) NOT NULL COMMENT '用户名',
@@ -15,9 +21,10 @@ CREATE TABLE `user` (
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`),
   UNIQUE `username`(`username`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='用户表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
 
 -- 角色表
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `name` VARCHAR(16) NOT NULL COMMENT '名称',
@@ -29,9 +36,10 @@ CREATE TABLE `role` (
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='角色表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
 -- 用户角色关联表
+DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `user_id` INT(16) UNSIGNED NOT NULL COMMENT '用户Id',
@@ -41,14 +49,15 @@ CREATE TABLE `user_role` (
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='用户角色关联表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色关联表';
 
 -- 菜单表
+DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `type` TINYINT UNSIGNED NOT NULL COMMENT '类型 1-菜单 2-iframe 3-外链 4-按钮',
   `parent_id` INT(16) UNSIGNED NOT NULL DEFAULT 0 COMMENT '上级菜单Id',
-  `title` VARCHAR(16) NOT NULL COMMENT '标题',
+  `title` VARCHAR(32) NOT NULL COMMENT '标题',
   `name` VARCHAR(32) NULL COMMENT '路由名称',
   `path` VARCHAR(255) NULL COMMENT '路由路径',
   `component` VARCHAR(255) NULL COMMENT '组件路径',
@@ -72,9 +81,10 @@ CREATE TABLE `menu` (
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='菜单表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
 
 -- 角色菜单关联表
+DROP TABLE IF EXISTS `role_menu`;
 CREATE TABLE `role_menu` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `role_id` INT(16) UNSIGNED NOT NULL COMMENT '角色Id',
@@ -84,9 +94,10 @@ CREATE TABLE `role_menu` (
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='角色菜单关联表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='角色菜单关联表';
 
 -- 部门表
+DROP TABLE IF EXISTS `dept`;
 CREATE TABLE `dept` (
   `id` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `parent_id` INT(16) UNSIGNED NOT NULL DEFAULT 0 COMMENT '上级部门Id',
@@ -100,4 +111,35 @@ CREATE TABLE `dept` (
   `last_modified_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   `last_modified_by` VARCHAR(32) NOT NULL DEFAULT 'system' COMMENT '最后修改人账号',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8 COMMENT='部门表';
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='部门表';
+
+
+-- 初始数据
+INSERT INTO `dept` VALUES (1, 0, '丐帮', 1, 0, 1, '', '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+
+INSERT INTO `user` VALUES (1, 'admin', 'admin', 0, 1, '13411111111', '', '{noop}admin123', 1, '2024-09-15 00:00:00', 'system', '2024-09-19 13:03:36', 'system');
+INSERT INTO `user` VALUES (2, 'common', 'common', 0, 1, '13411111112', '', '{noop}admin123', 1, '2024-09-15 00:00:00', 'system', '2024-09-19 13:03:39', 'system');
+
+INSERT INTO `role` VALUES (1, '超级管理员', 'system', '超级管理员拥有最高权限', 1, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role` VALUES (2, '普通', 'common', '普通角色', 1, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+
+INSERT INTO `menu` VALUES (1, 0, 0, '权限管理', 'Permission', '/permission', '', 'ep:lollipop', '', '', '', '', '', '', '', 0, 0, 0, 0, 1, 0, 10, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (2, 0, 1, '页面权限', 'PermissionPage', '/permission/page/index', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (3, 0, 1, '按钮权限', 'PermissionButtonRouter', '/permission/button', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (4, 0, 3, '路由返回按钮权限', 'PermissionButtonRouter', '/permission/button/router', 'permission/button/index', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (5, 0, 3, '登录接口返回按钮权限', 'PermissionButtonLogin', '/permission/button/login', 'permission/button/perms', '', '', '', '', '', '', '', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (6, 3, 4, 'permission:btn:add', '', '', '', '', '', '', '', '', '', 'permission:btn:add', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (7, 3, 4, 'permission:btn:edit', '', '', '', '', '', '', '', '', '', 'permission:btn:edit', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `menu` VALUES (8, 3, 4, 'permission:btn:delete', '', '', '', '', '', '', '', '', '', 'permission:btn:delete', '', 0, 0, 0, 0, 1, 0, 99, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+
+INSERT INTO `user_role` VALUES (1, 1, 1, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `user_role` VALUES (2, 2, 2, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+
+INSERT INTO `role_menu` VALUES (1, 1, 1, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (2, 1, 2, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (3, 1, 3, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (4, 1, 4, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (5, 1, 5, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (6, 1, 6, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (7, 1, 7, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
+INSERT INTO `role_menu` VALUES (8, 1, 8, '2024-09-15 00:00:00', 'system', '2024-09-15 00:00:00', 'system');
